db1974afde503bea4bf5c5fc6089195ea0379216
Mahbod <mahbodnouri@gmail.com>
2021-12-22 23:46:08 +0000

    debug connections

--- src/connection.jl
+++ src/connection.jl
@@ -12,7 +12,8 @@ function randomConnection(n::Int, p::Float64; weight_min = 0, weight_max= 1, see
     end
     adjacency = rand(n, n) .> p
     adjacency -= adjacency .* (zeros(n ,n) + I) # remove recurrent connections
-    weights = rand(n, n) .* adjacency .* (weight_max - weight_min) .+ weight_min
+    weights = rand(n, n) .* (weight_max - weight_min) .+ weight_min
+    weights .*= adjacency
     return Connection(weights, adjacency)
 end
 
@@ -21,6 +22,7 @@ function randomConnection(n_source::Int, n_target::Int, p::Float64; weight_min =
         Random.seed!(seed)
     end
     adjacency = rand(n_source, n_target) .> p
-    weights = rand(n_source, n_target) .* adjacency .* (weight_max - weight_min) .+ weight_min
+    weights = rand(n_source, n_target) .* (weight_max - weight_min) .+ weight_min
+    weights .*= adjacency
     return Connection(weights, adjacency)
 end
