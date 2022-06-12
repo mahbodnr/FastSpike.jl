0bc798b28ef5cabf50833821237fce88afc15886
Mahbod <mahbodnouri@gmail.com>
2022-06-12 14:20:54 +0100

    remove logging (not working)

--- src/FastSpike.jl
+++ src/FastSpike.jl
@@ -1,7 +1,5 @@
 module FastSpike
 
-using Logging
-Logging.disable_logging(Logging.Info) # ignore "info" logs
 import Base.@kwdef
 using LinearAlgebra
 using Random
@@ -11,8 +9,6 @@ using NNlib
 using CUDA
 using Adapt
 using OMEinsum
-Logging.disable_logging(Logging.Debug)
-
 
 export add_group!, connect!, run!, train!, reset!, pad1D, pad2D, record!, get, save, load,
         gpu, cpu, randomConnection, regularActivity, randomActivity, makeInput, EI,
