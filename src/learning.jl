756705ed1a121d09d239788d0d163446670f6086
Mahbod <mahbodnouri@gmail.com>
2022-07-24 13:04:06 +0100

    remove random noise from cSTDP

--- src/learning.jl
+++ src/learning.jl
@@ -77,16 +77,15 @@ function train!(network::SpikingNetwork, learning_rule::cSTDP)
     learning_rule.calcium .+= network.neurons.dt / learning_rule.τ_calcium .* (-learning_rule.calcium)
     learning_rule.calcium[network.spikes[1, :], :] .+= learning_rule.Cₚᵣₑ
     learning_rule.calcium[:, network.spikes[1, :]] .+= learning_rule.Cₚₒₛₜ
-    Gaussian = similar(learning_rule.calcium)
-    Gaussian .= rand(Normal(0, 1), size(Gaussian)...)
     learning_rule.efficacy = network.neurons.dt / learning_rule.τᵨ .* (
         -learning_rule.efficacy .* (1 .- learning_rule.efficacy) .* (learning_rule.ρ_star .- learning_rule.efficacy)
         +
         learning_rule.γ₊ .* (1 .- learning_rule.efficacy) .* Θ.(learning_rule.calcium .- learning_rule.θ₊)
         -
         learning_rule.γ₋ .* learning_rule.efficacy .* Θ.(learning_rule.calcium .- learning_rule.θ₋)
-        +
-        learning_rule.σ .* sqrt(learning_rule.τᵨ) .* Θ.(learning_rule.calcium .- min(learning_rule.θ₋, learning_rule.θ₊)) * Gaussian
+        # TODO: make random noise compatible with gpu.
+        # +
+        # learning_rule.σ .* sqrt(learning_rule.τᵨ) .* Θ.(learning_rule.calcium .- min(learning_rule.θ₋, learning_rule.θ₊)) * rand(Normal(0, 1), size(Gaussian)...)
     )
     network.weight = learning_rule.initial_weights .* learning_rule.efficacy
     network.weight = clamp.(network.weight, learning_rule.min_weight, learning_rule.max_weight)
